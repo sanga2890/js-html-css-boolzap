37521e0565699e69992784877241372ae1f1d3ff
Stefano <stefano2890@gmail.com>
2020-05-10 22:59:16 +0200

    A little more style

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-$('.contact-chat-left p.contact-name').append('Tizio')
-
+// Mi aggancio a tutti i <p> con classe last-message e aggiungo la stessa frase per ciascuno;
 $('.contact-chat-left p.last-message').append('blblablbllbalallaballabla')
 
+// Mi aggancio a tutti i <p> con classe time, figli del <div> con classe contact-chat-right e aggiungo lo stesso contenuto per ciascuno;
 $('.contact-chat-right p.time').append('Never')
