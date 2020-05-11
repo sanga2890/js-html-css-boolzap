ac6003abfb09ba0ebbb160b029c3234445836d23
Stefano <stefano2890@gmail.com>
2020-05-11 19:09:09 +0200

    aggiunto inserimento testo utente

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ $('.contact-chat-left p.last-message').append('blblablbllbalallaballabla')
 
 // Mi aggancio a tutti i <p> con classe time, figli del <div> con classe contact-chat-right e aggiungo lo stesso contenuto per ciascuno;
 $('.contact-chat-right p.time').append('Never')
+
+// per il momento aggancio il clic sul microfono come se fosse il tasto invio messaggio e vado a leggere il testo contenuto nell'input con id="input-utente" e lo assegno ad una variabile;
+$('.write-message .material-icons').click(function(){
+    var insert_text = $('#input-utente').val();
+    var new_message = $('.template .my-message').clone();
+    new_message.find('p.mex').text(insert_text);
+    new_message.find('p.time').text('0.00');
+    $('#conversation').append(new_message);
+    
+})
